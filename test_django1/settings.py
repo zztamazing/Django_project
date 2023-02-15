07023638c1c31eea1890d1716e51a39dbd2dd674
zztamazing <zztpoo@gmail.com>
2023-02-15 09:47:52 +0800

    block email address and application password

--- test_django1/settings.py
+++ test_django1/settings.py
@@ -166,8 +166,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'zztpoo@gmail.com'
-EMAIL_HOST_PASSWORD = 'fxneoanconbgrgpw'
+EMAIL_HOST_USER = 'xxxxxxxx@gmail.com'
+EMAIL_HOST_PASSWORD = 'xxxxxxxxxxxxxx'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
